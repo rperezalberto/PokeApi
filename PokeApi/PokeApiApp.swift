dbc83b5dd1f70ff8affc3838b84d05ca7c8f589b
Robin Perez <35049749+rperezalberto@users.noreply.github.com>
2024-12-14 19:01:43 -0400

    Initial Commit

---
+++ PokeApi/PokeApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeApiApp.swift
+//  PokeApi
+//
+//  Created by Robin Perez on 14/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PokeApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
